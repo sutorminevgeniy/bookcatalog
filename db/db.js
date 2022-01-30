04b75da6bb8d418a592eab152051a376ac78fd54
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2022-01-31 01:57:39 +0300

    include sqlite data base

---
+++ db/db.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+// Настройка соединения с базой
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db/database.sqlite'
+});
+
+// Проверка соединения с базой
+async function run() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+run();
+
+// закрытие соединения
+// sequelize.close()
