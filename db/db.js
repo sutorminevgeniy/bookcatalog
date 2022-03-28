1e51abeedc08608bc19910ee7fd888275db8345a
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2022-03-29 00:41:58 +0300

    update db

--- db/db.js
+++ db/db.js
@@ -10,41 +10,13 @@ const sequelize = new Sequelize({
   storage: './db/database.sqlite'
 });
 
-// Создание моделей таблиц
-const Model = Sequelize.Model;
 
-class Author extends Model {}
-Author.init({
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false // allowNull defaults to true
-  },
-  middleName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  sequelize,
-  modelName: 'Author'
-});
 
 // Проверка соединения с базой
 async function run() {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-
-    // await sequelize.sync();
-    await sequelize.sync({force: true});
-    console.log('Tables sync.');
-
-    // создание записей
-    const author = await Author.create({firstName: 'Фёдор', middleName: 'Михайлович', lastName: 'Достоевский'})
-    console.log(author.toJSON())
-    console.log(JSON.stringify(author, null, 2))
-    const authors = await Author.bulkCreate(authorRes);
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
